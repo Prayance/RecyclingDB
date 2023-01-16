29e40b7e03fc8fc8daeef477b2e2ea76695e0b51
Prayance <elisavet.andrikopoulou@port.ac.uk>
2023-01-16 14:14:43 +0000

    ini

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,8 +1,33 @@
+-- Create a new database called 'DatabaseName'
+-- Connect to the 'master' database to run this snippet
+USE master
+GO
+-- Create the new database if it does not exist already
+IF NOT EXISTS (
+	SELECT name
+		FROM sys.databases
+		WHERE name = N'RecyclingDB'
+)
+CREATE DATABASE [RecyclingDB]
+GO
 
+-- Use the recycling database
+USE RecyclingDB
+GO
 
--- Create Dimensions --
-Create table WasteTypeDim (
-	WasteTypeID int PRIMARY KEY,
-	Restriction VARCHAR(26),
-	Name VARCHAR(50)
+-- Create a new table called 'WasteTypeDim'
+-- Drop the table if it already exists
+IF OBJECT_ID('WasteTypeDim', 'U') IS NOT NULL
+DROP TABLE WasteTypeDim
+GO
+-- Create the table in the specified schema
+CREATE TABLE WasteTypeDim
+(
+	WasteTypeID INT NOT NULL PRIMARY KEY, -- primary key column
+	Restriction [NVARCHAR](50),
+	WasteName [NVARCHAR](50)
+
 );
+GO
+
+
